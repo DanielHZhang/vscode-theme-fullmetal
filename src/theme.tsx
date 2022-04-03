3e634b124d88c541a30214444ac3f9fa2a72f39c
Daniel Zhang <30360288+DanielHZhang@users.noreply.github.com>
2022-04-03 17:29:48 -0400

    fix: add fat arrow operator for rust

--- src/theme.tsx
+++ src/theme.tsx
@@ -348,6 +348,7 @@ const theme: ColorTheme = {
         specificity('source', 'keyword.other'),
         specificity('source', 'keyword.control'),
         'keyword.declaration',
+        'keyword.operator.arrow.fat',
         'keyword.operator.expression',
         'keyword.operator.new',
         'support.type.object.module',
