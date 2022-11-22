d0baa3e44e0e8560ba6378168692316715340bbd
Daniel Zhang <30360288+DanielHZhang@users.noreply.github.com>
2022-11-21 23:01:45 -0500

    fix: directly pass in tag name to action

--- .github/scripts/get-version.js
+++ .github/scripts/get-version.js
@@ -1,11 +1,10 @@
 const semver = require('semver');
 
-module.exports = ({github, core}) => {
-  const releaseTag = github.event.release.tag_name;
-  const result = semver.coerce(releaseTag);
+module.exports = ({core, tag}) => {
+  const result = semver.coerce(tag);
 
   if (!result) {
-    core.error(`Could not coerce release tag ${releaseTag} into semver.`);
+    core.error(`Could not coerce release tag ${tag} into semver.`);
     process.exit(1);
   }
 
